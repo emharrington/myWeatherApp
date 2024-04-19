716218265c70235883f4bc157000f66b61900e0e
emharrington <emilyharrington33@gmail.com>
2024-04-19 16:51:36 +0100

    If else blocked refactored into separate file

--- src/Components/Components/SearchComponent.jsx
+++ src/Components/Components/SearchComponent.jsx
@@ -1,10 +1,8 @@
 import React, {useState} from 'react';
+import {iconMap} from '../Assets/IconMap';
 import search_icon from "../Assets/search.png"
 import clear_icon from "../Assets/clear.png";
 import cloud_icon from "../Assets/cloud.png";
-import drizzle_icon from "../Assets/drizzle.png";
-import rain_icon from "../Assets/rain.png";
-import snow_icon from "../Assets/snow.png";
 
 function SearchComponent() {
     let api_key = "af5ab7e27b93af99ad971d7fd7f8f7f6";
@@ -33,38 +31,9 @@ function SearchComponent() {
         temperature[0].innerHTML = Math.floor(data.main.temp)+" °c";
         location[0].innerHTML = data.name;
 
-        if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n")
-        {
-            setWicon(clear_icon);
-        } 
-        else if(data.weather[0].icon==="02d" || data.weather[0].icon==="02n")
-        {
-            setWicon(cloud_icon);
-        }
-        else if(data.weather[0].icon==="03d" || data.weather[0].icon==="03n")
-        {
-            setWicon(drizzle_icon);
-        }
-        else if(data.weather[0].icon==="04d" || data.weather[0].icon==="04n")
-        {
-            setWicon(drizzle_icon);
-        }
-        else if(data.weather[0].icon==="09d" || data.weather[0].icon==="09n")
-        {
-            setWicon(rain_icon);
-        }
-        else if(data.weather[0].icon==="10d" || data.weather[0].icon==="10n")
-        {
-            setWicon(rain_icon);
-        }
-        else if(data.weather[0].icon==="13d" || data.weather[0].icon==="13n")
-        {
-            setWicon(snow_icon);
-        }
-        else{
-            setWicon(clear_icon);
-        }
-
+        const weatherIcon = data.weather[0].icon;
+        const selectedIcon = iconMap[weatherIcon] || clear_icon;
+        setWicon(selectedIcon);
     }    
 
     return (
